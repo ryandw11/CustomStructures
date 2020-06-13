ef934f200e4fd9950131b950c00e13f680bdcdcc
Ryandw11 <6239385+ryandw11@users.noreply.github.com>
2020-06-13 03:10:50 -0700

    Bug Fixes

--- src/com/ryandw11/structure/listener/ChunkLoad.java
+++ src/com/ryandw11/structure/listener/ChunkLoad.java
@@ -12,46 +12,43 @@ import com.ryandw11.structure.utils.StructurePicker;
 
 /**
  * Class for when a chunk loads.
- * @author Ryandw11
  *
+ * @author Ryandw11
  */
-public class ChunkLoad implements Listener{
-	
-	//private CustomStructures plugin;
-	
-	public ChunkLoad(){
-		//this.plugin = CustomStructures.plugin;
-	}
-	
-	@EventHandler
-	public void loadevent(ChunkLoadEvent e){
-		if(!CustomStructures.enabled) return;
-		
-		if(e.isNewChunk()){ //Checks to see if the chunk is new or an old one.
-			World w = e.getChunk().getWorld(); //Grabs the world
-			Block b = e.getChunk().getBlock(0, 5, 0); //Grabs the block 0, 5, 0 in that chunk.
+public class ChunkLoad implements Listener {
 
-			boolean foundLand = false; //True when the block selected is an ideal place for a structure.
-			if(w.getHighestBlockYAt(b.getX(), b.getZ()) == -1) return;
-			Block bb = e.getChunk().getBlock(0, w.getHighestBlockYAt(b.getX(), b.getZ()), 0); //grabs the highest block in that chunk at X = 0 and Z = 0 for that chunk.
-			int trys = 0;
-			while (!foundLand){//While land was not found it keeps checking.
-				if(trys >= 20) return; //added anti crash.
-				if(bb.getType() != Material.AIR){
-					foundLand = true;
-				}
-				else{
-					bb = bb.getLocation().subtract(0, 1, 0).getBlock();
-				}
-				trys++;
-			}
-			
-			/*
-			 * Schematic handeler
-			 * Runs a BukkitRunnable to prevent the server from crashing.
-			 */
-			StructurePicker s = new StructurePicker(bb, e.getChunk(), CustomStructures.getInstance());
-			s.runTaskTimer(CustomStructures.plugin, 1, 10);
-		}
-	}
+    public ChunkLoad() {
+    }
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent e) {
+        if (!CustomStructures.enabled) return;
+
+        if (!e.isNewChunk()) return;
+
+
+        World w = e.getChunk().getWorld(); //Grabs the world
+        Block b = e.getChunk().getBlock(0, 5, 0); //Grabs the block 0, 5, 0 in that chunk.
+
+        boolean foundLand = false; //True when the block selected is an ideal place for a structure.
+        if (w.getHighestBlockYAt(b.getX(), b.getZ()) == -1) return;
+        Block bb = e.getChunk().getBlock(0, w.getHighestBlockYAt(b.getX(), b.getZ()), 0); //grabs the highest block in that chunk at X = 0 and Z = 0 for that chunk.
+        int trys = 0;
+        while (!foundLand) {//While land was not found it keeps checking.
+            if (trys >= 20) return; //added anti crash.
+            if (bb.getType() != Material.AIR) {
+                foundLand = true;
+            } else {
+                bb = bb.getLocation().subtract(0, 1, 0).getBlock();
+            }
+            trys++;
+        }
+
+        /*
+         * Schematic handler
+         * This activity is done async to prevent the server from lagging.
+         */
+        StructurePicker s = new StructurePicker(bb, e.getChunk(), CustomStructures.getInstance());
+        s.runTaskTimer(CustomStructures.plugin, 1, 10);
+    }
 }
