9898d54dea60e26cc819b21f96b471d718c60db0
Ryandw11 <no-reply@ryandw11.com>
2020-12-30 15:17:26 -0700

    Allow the plugin to spawn in the void.

--- src/main/java/com/ryandw11/structure/listener/ChunkLoad.java
+++ src/main/java/com/ryandw11/structure/listener/ChunkLoad.java
@@ -35,18 +35,22 @@ public class ChunkLoad implements Listener {
         Block b = e.getChunk().getBlock(0, 5, 0); //Grabs the block 0, 5, 0 in that chunk.
 
         boolean foundLand = false; //True when the block selected is an ideal place for a structure.
-        if (w.getHighestBlockYAt(b.getX(), b.getZ()) == -1) return;
-        Block bb = e.getChunk().getBlock(0, w.getHighestBlockYAt(b.getX(), b.getZ()), 0); //grabs the highest block in that chunk at X = 0 and Z = 0 for that chunk.
-        int trys = 0;
-        while (!foundLand) {//While land was not found it keeps checking.
-            if (trys >= 20) return; //added anti crash.
-            if (bb.getType() != Material.AIR) {
-                foundLand = true;
-            } else {
-                bb = bb.getLocation().subtract(0, 1, 0).getBlock();
-            }
-            trys++;
-        }
+        Block bb;
+        if (w.getHighestBlockYAt(b.getX(), b.getZ()) != -1)
+            bb = e.getChunk().getBlock(0, w.getHighestBlockYAt(b.getX(), b.getZ()), 0); //grabs the highest block in that chunk at X = 0 and Z = 0 for that chunk.
+        else
+            bb = e.getChunk().getBlock(0, 20, 0);
+
+//        int trys = 0;
+//        while (!foundLand) {//While land was not found it keeps checking.
+//            if (trys >= 20) return; //added anti crash.
+//            if (bb.getType() != Material.AIR) {
+//                foundLand = true;
+//            } else {
+//                bb = bb.getLocation().subtract(0, 1, 0).getBlock();
+//            }
+//            trys++;
+//        }
 
         /*
          * Schematic handler
